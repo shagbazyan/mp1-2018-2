e5d34426705903e02cae0490c4b69276bb635417
Hrenorubka <43463620+Hrenorubka@users.noreply.github.com>
2018-09-21 23:04:48 +0300

    Здесь написать ваш комментарий к коммиту 2

---
+++ ermolaev.ia/task0/Source.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+//#include <locate.h>
+
+void main()
+{
+	//setlocale(LC_ALL, "Russian");
+	int x1, y1, r1, x2, y2, r2;
+	printf("x1, y1, r1\n");
+	scanf_s("%d" "%d" "%d", &x1, &y1, &r1);
+	printf("x2, y2, r2\n");
+	scanf_s("%d" "%d" "%d", &x2, &y2, &r2);
+	float rast = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	if (rast > r1 + r2)
+	{
+		printf("Ne peresek");
+	}
+	else if (rast == r1 + r2)
+	{
+		printf("Kacanie");
+	}
+	else if ((rast < r1 + r2) && (x1 == x2) && (y1 == y2) && (r1 == r2))
+	{
+		printf("Sovpadenie");
+	}
+	else if ((rast < r1 + r2) && ((r2 >= r1) && (x1*x1 + y1 * y1 > r2*r2) || (r1 >= r2) && (x2*x2 + y2 * y2 >= r1 * r1)))
+	{
+		printf("Peresek");
+	}
+	else if ((rast < r1 + r2) && ((r2 >= r1) && (x1*x1 + y1 * y1 < r2*r2) || (r1 >= r2) && (x2*x2 + y2 * y2 < r1*r1)))
+	{
+		if ((r1 >= r2) && ((r1 - rast) == r2) || (r1 <= r2) && ((r2 - rast) == r1))
+			printf("Kasanie");
+		else if ((r1 >= r2) && ((r1 - rast) > r2) || (r1 <= r2) && ((r2 - rast) > r1))
+			printf("Okr v Okr");
+		else if ((r1 >= r2) && ((r1 - rast) < r2) || (r1 <= r2) && ((r2 - rast) < r1))
+			printf("Peresechenie");
+	}
+	while (true);
+
+}
