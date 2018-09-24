52b3d6b59848f79f866538a8466bc2100c4f3397
Ilya <pokiloda@mail.ru>
2018-09-25 00:06:04 +0300

    corrected solution for task1

--- mishin_ia/task1/Source.cpp
+++ mishin_ia/task1/Source.cpp
@@ -2,19 +2,26 @@
 
 int main() {
 	int width, heigth, deep;
-	float densityDSP = 3.2, densityDVP = 3;// г/см^3
-	int volumeDSP, volumeDVP;
+	float densityDSP = 0.8, densityDVP = 0.75, densityWood = 0.5;// г/см^3
+	int volumeDSP, volumeDVP, volumeWood;
 	float weight;
 	scanf_s("%d,%d,%d", &heigth, &width, &deep);
+
+	width *= 10;
+	heigth *= 10;
+	deep *= 10;
 
 	volumeDVP = width * heigth * 5;
 	volumeDSP = 2 * heigth*deep * 15;
 	volumeDSP += 2 * width*deep * 15;
 	volumeDSP += width * heigth * 10;
-	volumeDSP += width * deep * 15 * ((heigth - 1) / 40);
+	volumeWood = width * deep * 15 * ((heigth - 15) / 400);
+	
 	volumeDSP /= 1000;// перевод в см^3
 	volumeDVP /= 1000;
-	weight = volumeDSP * densityDSP + volumeDVP * volumeDVP;
+	volumeWood /= 1000;
+	
+	weight = volumeDSP * densityDSP + volumeDVP * densityDVP + volumeWood * densityWood;
 
 	printf("%f", weight);
 	scanf_s("%d", &width);
