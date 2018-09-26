866ecf8ee7de7aff80186189ecdf0f4e590760a8
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2018-09-26 23:27:44 +0300

    Поправил единицы измерения

--- kolesnikov_gr/task1/Source.cpp
+++ kolesnikov_gr/task1/Source.cpp
@@ -16,34 +16,34 @@ int main()
 	float Mzstenki, Mdver, Mbok, Mvnutr, Mkrish, Mpolok, Mobsh;
 	float a;
 
-	printf("Введите высоту(h), ширину(w) и глубину(d) в сантиметрах\n");
+	printf("Vvedite visoty(h), shiriny(w) i glubiny(d) v sanrimetrah\n");
 	scanf_s("%f%f%f", &H, &W, &D);
-	printf("Введите плотность ДЛП, ДВП и дерева в граммах на сантиметрах кубических\n");
+	printf("Vvedite plotnost DSP, DVP i dereva v kilogrammah na santimetri kybi4eskie\n");
 	scanf_s("%f%f%f", &pDSP, &pDVP, &pDEREVA);
-	printf("Введите толщину задней стенки в сантиметрах\n");
+	printf("Vvedite tolshiny zadnei stenki v sanrimetrah\n");
 	scanf_s("%f", &Tzstenki);
-	printf("Введите толщину боковой стенки в сантиметрах\n");
+	printf("Vvedite tolshiny bokovoi stenki v sanrimetrah\n");
 	scanf_s("%f", &Tbok);
-	printf("Введите толщину двери в сантиметрах\n");
+	printf("Vvedite tolshiny dveri v sanrimetrah\n");
 	scanf_s("%f", &Tdver);
-	printf("Введите толщину полок в сантиметрах\n");
+	printf("Vvedite tolshiny polok v sanrimetrah\n");
 	scanf_s("%f", &Tpolok);
-	printf("Введите толщину крышек в сантиметрах\n");
+	printf("Vvedite tolshiny krishek v sanrimetrah\n");
 	scanf_s("%f", &Tkrish);
 
 	Mzstenki = pDVP * H*W*Tzstenki;
-	printf("Масса задней стенки %f\n", Mzstenki);
+	printf("Massa zadnei stenki %f\n", Mzstenki);
 	Mbok = 2 * pDSP*H*D*Tbok;
-	printf("Масса боковин %f\n", Mbok);
+	printf("Massa bokovin %f\n", Mbok);
 	Mkrish = 2 * pDSP*W*D*Tkrish;
-	printf("Масса крышек %f\n", Mkrish);
+	printf("Massa krishek %f\n", Mkrish);
 	Mdver = pDEREVA * H*W*Tdver;
-	printf("Масса дверей %f\n", Mdver);
+	printf("Massa dverei %f\n", Mdver);
 	Npolok = (int)((H - 2 * Tkrish - 00000.1) / 40);
-	printf("Количество полок %f\n", Npolok);
+	printf("Koli4estvo polok %f\n", Npolok);
 	Mpolok = Npolok * pDSP*W*D*Tpolok;
-	printf("Масса полок %f\n", Mpolok);
+	printf("Massa polok %f\n", Mpolok);
 	Mobsh = Mzstenki + Mbok + Mkrish + Mdver + Mpolok;
-	printf("Общая масса шкафа %f\n", Mobsh);
+	printf("Obshaya Massa Shkafa %5.1f kilogramm\n", Mobsh);
 	scanf_s("%f", &a);
 }
