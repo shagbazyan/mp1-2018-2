152b262fcde13fc4223d335d073707e5c31819f9
Python3x <biathloner2@mail.ru>
2018-09-28 00:33:59 +0300

    Task 0 is ready!!!

--- gerasimov_ma/task1/Source.cpp
+++ gerasimov_ma/task1/Source.cpp
@@ -11,28 +11,36 @@
 
 int main()
 {
-	int height, width, depth;
-	int reminder, quantity, shelf_thickness;
+	float height, width, depth;
+	float shelf_thickness;
+	int reminder, quantity;
 	float mass, back_mass, side_mass, cover_mass, door_mass, shelf_mass;
 	int another;
 
 	do
 	{
 		printf("Enter the height, width and depth of the wardrobe in cm\n");
-		scanf_s("%d %d %d", &height, &width, &depth);
+		scanf_s("%f %f %f", &height, &width, &depth);
 		printf("Enter the thickness of the shelves in mm\n");
-		scanf_s("%d", &shelf_thickness);
+		scanf_s("%f", &shelf_thickness);
 
-		reminder = (height * 10) % 400;
-		quantity = height * 10 / 400;
+		//Считаем количество полок
+		reminder = (int(height * 10)) % 400;
+		quantity = int(height * 10 / 400);
 		if (reminder < quantity * shelf_thickness)
 			quantity--;
 
-		back_mass = DVP * (height / 100.0) * (width / 100.0) * BACK_THICKNESS;
-		side_mass = DSP * (height / 100.0) * (depth / 100.0) * SIDE_THICKNESS;
-		cover_mass = DSP * (width / 100.0) * (depth / 100.0) * COVER_THICKNESS;
-		door_mass = WOOD * (height / 100.0) * (width / 100.0) * DOOR_THICKNESS;
-		shelf_mass = DSP * ((width / 100.0) - 2 * SIDE_THICKNESS) * ((depth / 100.0) - BACK_THICKNESS - DOOR_THICKNESS) * (shelf_thickness / 1000.0);
+		//Переводим см и мм в м
+		height /= 100.0;
+		width /= 100.0;
+		depth /= 100.0;
+		shelf_thickness /= 1000.0;
+
+		back_mass = DVP * height * width * BACK_THICKNESS;
+		side_mass = DSP * height * depth * SIDE_THICKNESS;
+		cover_mass = DSP * width * depth * COVER_THICKNESS;
+		door_mass = WOOD * height * width * DOOR_THICKNESS;
+		shelf_mass = DSP * (width - 2 * SIDE_THICKNESS) * (depth - BACK_THICKNESS - DOOR_THICKNESS) * shelf_thickness;
 
 		mass = back_mass + 2 * side_mass + 2 * cover_mass + door_mass + quantity * shelf_mass;
 		printf("\n");
