fd87af51fa1d1cc94e1da02e2df6dfdb31fe7a8b
Мишин Илья Александрович <mishin.i@ccam.cmc.unn.net>
2018-09-28 10:32:39 +0300

    first solution for task2

---
+++ mishin_ia/task2/Source.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+
+int main() {
+
+	srand(time(NULL));
+	int game, attemps = 0;
+	printf("Choose game: write 1 for first mode, 2 for second mode \n");
+	scanf_s("%d", &game);
+	printf("Start\nEnter number between 1 and 1000 \n");
+
+	if (game == 1) {
+		int r;
+		int player;
+		r = rand() % 10 + 1;
+		short int flag = 0;
+			
+		while (flag == 0) {
+			attemps++;
+			scanf_s("%d", &player);
+			if (player == r) {
+				printf("You win\n");
+				flag = 1;
+			}
+			else
+			{
+				if (player > r) {
+					printf("Yor number is higher then hidden number\n");
+				}
+				else
+				{
+					printf("Yor number is lower then hidden number\n");
+				}
+			}
+		}
+	
+		
+	}
+	else
+	{
+		int player,r, bottom = 1, up = 10;
+		scanf_s("%d", &player);
+		char point;
+		short int flag = 0;
+
+		while (flag == 0) {
+			attemps++;
+					
+			r = rand() % up + bottom;
+			printf("Is it number %d ?\n", r);
+			printf("%d,%d\n", up, bottom);
+			scanf_s("%c", &point);
+			
+			if (point == '=') {
+				printf("You win\n");
+				flag = 1;
+			}
+			else
+			{
+				if (point == '>') {
+					printf("Ok!\n");
+					bottom = r;
+					
+				}
+				if (point == '<')
+				{
+					printf("Ok!\n");
+					up = r;
+				}
+			}
+		}
+		
+
+	}
+	/*int i, r;
+
+	srand(time(NULL));
+	for (i = 0; i < 10; i++) {
+		r = rand()%1000;
+		printf("%d\n", r);
+	}
+	*/
+
+	printf("Your attemps: %d", attemps);
+	_getch();
+	return 0;
+
+}
