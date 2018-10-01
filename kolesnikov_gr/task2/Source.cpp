427d74f6aa0ff14c24ff250488a451d01cb87ccf
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2018-10-01 20:26:46 +0300

    Второй режим бетка

--- kolesnikov_gr/task2/Source.cpp
+++ kolesnikov_gr/task2/Source.cpp
@@ -10,20 +10,74 @@ int main()
 	srand(time(0));
 	int chislo;
 	int predpolozhenie;
-	
-	predpolozhenie = -1;
-	chislo = rand() % 1001;
-	printf("%i\n", chislo); //  для проверки
-	while (predpolozhenie != chislo)
+	int popitki;
+	int rejim;
+	int max;
+	int min;
+	int truth;
+	char neravenstvo;
+
+	printf("Выберите режим игры: нажмите 1, чтобы угадывать число; нажми 2, чтобы загадывать\n");
+	scanf_s("%d", &rejim);
+	if (rejim > 2)
+		printf("Такого режима еще нет(пока что)\n");
+	if (rejim == 1)
 	{
-		printf("Введите ваше предположение\n");
-		scanf_s("%d", &predpolozhenie);
-		if (predpolozhenie == chislo)
-			printf("Вы нашли загаданное число\n");
-		else if (predpolozhenie > chislo)
-			printf("Загаданное число меньше\n");
-		else printf("Загаданное число больше\n");
+		predpolozhenie = -1;
+		popitki = 0;
+		chislo = rand() % 1001;
+		printf("%i\n", chislo); //  для проверки
+		while (predpolozhenie != chislo)
+		{
+			popitki = popitki + 1;
+			printf("Введите ваше предположение\n");
+			scanf_s("%d", &predpolozhenie);
+			if (predpolozhenie == chislo)
+				printf("Вы нашли загаданное число. Количество попыток: %d \n", popitki);
+			else if (predpolozhenie > chislo)
+				printf("Загаданное число меньше\n");
+			else printf("Загаданное число больше\n");
+		}
 	}
 
-	_getch();
+	if (rejim == 2)
+	{
+		max = 1000;
+		min = 0;
+		truth = 0;
+		printf("Введите число\n");
+		scanf_s("%d", &chislo);
+		popitki = 0;
+		predpolozhenie = -1;
+		while (predpolozhenie != chislo)
+		{
+			popitki = popitki + 1;
+			predpolozhenie = rand () % (max-min) + min;
+			printf("Вы загадали число %d ?\n", predpolozhenie);
+			scanf_s("%c", &neravenstvo);
+			scanf_s("%c", &neravenstvo);
+			switch (neravenstvo)
+			{
+			    case('='):
+			    {
+				    printf("Ну я так и знал, что вы загадали именно это число. Количество попыток: %d", popitki);
+				    predpolozhenie = chislo;
+				    break;
+			    }
+			    case('>'):
+			    {
+				     min = predpolozhenie + 1;
+				     break;
+			     }
+			    case('<'):
+			    {
+				     max = predpolozhenie - 1;
+				     break;
+			    }
+			default:
+				break;
+			}
+		}
+	}
+		_getch();
 }
