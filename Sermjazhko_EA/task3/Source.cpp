73fa55644b5777b88f4210835c5b253a752594f7
Sermjazhko <43463670+Sermjazhko@users.noreply.github.com>
2018-10-13 16:38:48 +0300

    Доделала задание 3

--- Sermjazhko_EA/task3/Source.cpp
+++ Sermjazhko_EA/task3/Source.cpp
@@ -1,74 +1,82 @@
-#include <stdio.h>
-#include <locale.h>
-#include <time.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <locale.h> 
+#include <time.h> 
+#include <stdlib.h> 
 
-int main(void)
+void main()
 {
 	setlocale(LC_ALL, "Rus");
-	int n, ran, i, q, r; //количество цифр в числе, рандом
+	int n, i, mass[5], r1; //длина, счетчик, массив рандома, сравнение 
+	int kor, byk, mas[5], m, a, k, N=0; //коровы, быки, массив числа, остаток, само число, счетчик 
 
 	srand(time(NULL));
-	printf("Введите длину загадываемого числа (от 2 до 4)\n");
+	printf("Игра: Коровы и Быки!\n Введите длину загадываемого числа (от 2 до 5)\n");
 	scanf_s("%d", &n);
-	//слишком долго
-	if (n == 2)
-	{ 
-		ran = rand()%100;
-		i = ran;
-		q = i / 10;
-		r = i % 10;
-		while (q==r)//если при делении на 10 остаток и частное равны, то повтор
-		{
-			ran = rand() % 100;
-			i = ran;
-		}
-		printf("Число загадано. Напоминаю, что число двузначное, ваши предположения?\n");
-		scanf_s("%d%d", &i1, &j1);
-	}
-	else
+
+	while ((n < 2) || (n > 5))
 	{
-		if (n == 3)
-		{
-			ran1 = 1 + rand() % 9;
-			i = ran1;
-			ran2 = rand() % 10;
-			j = ran2;
-			ran3 = rand() % 10;
-			d = ran3;
-			while ((j == i) || (i == d) || (j == d))//если при делении на 100 частное, остаток при делении на 10 и остаток остатка равны, то повтор
-			{
-				ran2 = rand() % 10;
-				j = ran2;
-				ran3 = rand() % 10;
-				d = ran3;
-			}
-			printf("Число загадано. Напоминаю, что число трехзначное, ваши предположения?\n");
-			scanf_s("%d%d%d", &i1, &j1, &d1);
-		}
-		else
-		{
-			ran1 = 1 + rand() % 9;
-			i = ran1;
-			ran2 = rand() % 10;
-			j = ran2;
-			ran3 = rand() % 10;
-			d = ran3;
-			ran4 = rand() % 10;
-			l = ran4;
-			while ((j == i) || (i == d) || (j == d)||( i == l )||( j == l )||( d == l ))//если при делении на 1000 частное, остаток при делении на 100, остаток от остатка на 10 и последний ост. равны, то повтор
-			{
-				ran2 = rand() % 10;
-				j = ran2;
-				ran3 = rand() % 10;
-				d = ran3;
-				ran4 = rand() % 10;
-				l = ran4;
-			}
-			printf("Число загадано. Напоминаю, что число четырехзначное, ваши предположения?\n");
-			scanf_s("%d%d%d%d", &i1, &j1, &d1, &l1);
-		}
-
+		printf("Неверно введена длина загадываемого числа. Попробуйте ещё раз\n");
+		scanf_s("%d", &n);
 	}
 
+	r1 = 1;
+	while (r1 == 1)//1 jib,rf 
+	{
+		r1 = 0;
+		i = 0;
+		while (i < n)
+		{
+			if (i == 0)
+				mass[i] = 1 + rand() % 9;
+			else
+
+				mass[i] = rand() % 10;
+			i++;
+		}
+		for (i = 0; i < n; i++)
+			for (k = i + 1; k < n; k++) //2 jib 
+			{
+				//printf("!"); 
+				if (mass[i] == mass[k])
+				{
+					r1 = 1;
+					break;
+					//printf("%d", mass[i]); 
+				}
+				//printf("!"); 
+			}
+
+	}
+	byk = 0;
+	while (byk != n)
+	{
+		printf("Ваши предположения?\n");
+		scanf_s("%d", &a);
+		byk = 0;
+		kor = 0;
+		N++;
+		for (i = n - 1; i >= 0; i--)
+		{
+			m = a % 10;
+			mas[i] = m;
+			a = (a - m) / 10;
+		}
+		for (i = 0; i < n; i++)
+		{
+			if (mas[i] == mass[i])
+				byk++;
+			else
+			{
+				for (k = 0; k < n; k++)
+				{
+					if (mas[i] == mass[k])
+						kor++;
+				}
+			}
+		}
+		printf("Быков: %d, коров: %d \n", byk, kor);
+	}
+
+	printf("Поздравляю, вы победили! Количество попыток: %d\n", N);
+	scanf_s("%d", a);
 }
