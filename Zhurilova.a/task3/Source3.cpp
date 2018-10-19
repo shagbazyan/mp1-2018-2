49b76ef645fbd6b245927e434128105a89a238ac
Zhurilovaa <Anastasiya.Zhurilova@yandex.ru>
2018-10-19 22:26:45 +0300

    Исправленное task3

--- Zhurilova.a/task3/Source3.cpp
+++ Zhurilova.a/task3/Source3.cpp
@@ -9,75 +9,132 @@ void main()
 {
 	setlocale(LC_ALL, "Rus");
 
-	int n, proverka1=0, zag=0, i, j=10,  proverka2=0, otg, dl=0, kolK=0, kolB=0, x, r=1,q;
-	int cfr[] = { 0,1,2,3,4,5,6,7,8,9};//Создание масссива с цифрами
-	int chisZ[5] = { 0 };//Создание массива для цифр загаданного числа
-	int chisO[5] = { 0 };//Создание массива для цифр отгадки
-	
-	printf("Выберите длину числа от 2 до 5\n");
-	scanf("%d", &n);
-	while ((n < 2) || (n > 5))
+	int exit;
+	int n, dl, kolK, kolB;
+	int x, y, i, j;
+	int vvod1=0, vvod2, covp, proverka, game;
+	int gen[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int chisO[5] = { 0 };
+	int chisZ[5] = { 0 };
+	int zag=0, otg, zapasO;
+
+	do
 	{
-		printf("Вы ввели недопустимую длину числа. Введите ещё раз\n");
+		printf("Введите длину числа от 2 до 5\n");
 		scanf("%d", &n);
 	}
+	while ((n < 2) || (n > 5));
 	
-	while (proverka1 == 0)
+	
+	y = 10;
+	
+	for (i = 1; i <= n; i++)
 	{
-
-		for (i = 0; i < n;)//Генерирование числа по цифрам
+		x = rand() % (10);
+		if (y != x)
 		{
-			x = rand() % (10);
-			if (j != x)
-			{
-				chisZ[i] = x;//Заполнение массива chisZ
-				j = x;
-				zag = zag + x * r;
-				r = r * 10;//Увеличение порядка
-				i++;
-			}
+			zag = zag * 10 + x;
+			y = x;
+			chisZ[i] = gen[x];
 		}
-		proverka1 = 1;//Число сгенерировано правильно
+
 	}
-
-	printf("Число загадано\n");
-
-		
-	while(proverka2==0)
+	game = 0;
+	while (game == 0)
 	{
-		while (dl != n)
+		while (vvod1 == 0)
 		{
-			printf("Введите предположительную отгадку, т.е. число с НЕПОВТОРЯЮЩИМИСЯ цифрами\n ВНИМАНИЕ! Длина числа(вашей отгадки) должна совпадать с выбранной ранее длиной загаданного числа\n");
-			scanf("%d", &otg);
-			for (i = 0; otg > 0; i++)//Заполнение массива chisO
+
+			printf("Введите число\nВНИМАНИЕ!!! Число должно быть той же длины, что вы ввели раньше\nЧисло должно быть с РАЗЛИЧНЫМИ ЦИФРАМИ!\n");
+			scanf("%otg", &otg);
+			zapasO = otg;
+			dl = 1;
+			
+			while(zapasO > 0)
 			{
-				chisO[i] = otg % 10;
-				otg = otg / 10;
+				zapasO = zapasO / 10;
 				dl++;
 			}
-		}
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
+			zapasO = otg;
+			for (i = 1; i <= dl; i++)
 			{
-				if (chisZ[i] == chisO[j])//Сравнение цифр чисел
+				chisO[i] = zapasO % 10;
+				zapasO = zapasO / 10;
+			}
+			if (dl != n)
+			{
+				printf("Сказано было ввести чиcло ОДИНАКОВОЙ длины!\n");
+				vvod2 = 0;
+			}
+			else
+				if (dl == n)
 				{
-					kolK++;
-					if (i == j)//Сравнение позиций одинаковых цифр
+					vvod2 = 1;
+				}
+
+			while (vvod2 == 1)
+			{
+				covp = 0;
+				for (i = 1; i <= dl; i++)
+				{
+					for (j = (i + 1); j <= dl; j++)
+					{
+						if (chisO[i] != chisO[j])
+						{
+							covp = 0;
+
+						}
+						else
+							if (chisO[i] == chisO[j])
+							{
+								covp = 1;
+								
+							}
+
+					}
+					if (covp = 1)
+					{
+						printf("Было сказано с РАЗЛИЧНЫМИ цифрами!\n");
+						vvod2 = 0;
+						
+					}
+					else
+						if (covp = 0)
+						{
+							printf("Молодец! Вы ввели число верно!!!");
+							proverka = 0;
+						}
+				}
+			}
+		}
+
+		if (proverka == 0)
+		{
+			kolK = 0;
+			kolB = 0;
+			for (i = 1; i <= n; i++)
+			{
+				for (j = 1; j <= n; j++)
+				{
+					if (chisZ[i] == chisO[j])
+					{
+						kolK++;
+					}
+					if ((chisZ[i] == chisO[j]) && (i == j))
 					{
 						kolB++;
 					}
-
-			     }
-		     }
+				}
+			}
+			printf("Количество коров %d, Количество быков %d\n", kolK, kolB);
+			if (kolB == n)
+			{
+				printf("Молодец, вы угадали число!");
+				proverka = 1;
+				game = 1;
+			}
 		}
-		printf("Количество коров %d\n Количество быков %d\n", kolK, kolB);
-		if (kolB==n)
-		{
-			printf("Вы угадали число полностью!");
-			proverka2 = 1;//Окончание игры
-		}
-		dl = 0; kolK = 0; kolB = 0;//Обнуление переменных для входа в  while и for
 	}
-	scanf("%d", &q);
+	
+	scanf("%d", &exit);
 }
