b7570286d4ecb1f62c271616e04357a706a3a5fc
Python3x <biathloner2@mail.ru>
2018-10-20 23:03:13 +0300

    vse

--- gerasimov_ma/task3/Source.cpp
+++ gerasimov_ma/task3/Source.cpp
@@ -49,10 +49,31 @@ void biks_and_cows(int scanned, int length)
 	printf("Biks = %d; Cows = %d\n", biks, cows);
 }
 
+bool scanned_check(int scanned)
+{
+	int mas[10] = { 0 };
+
+	while (scanned > 0)
+	{
+		mas[scanned % 10]++;
+		scanned /= 10;
+	}
+	bool flag = true;
+	int i = 0;
+	while ((i <= 9) && (flag))
+	{
+		if (mas[i] > 1)
+			flag = false;
+		i++;
+	}
+	return flag;
+}
+
 int main()
 {
 	int length, number, scanned, attempts, tmp, i, another;
 	int number_digits[10][2];
+	bool flag;
 
 	do
 	{
@@ -61,7 +82,7 @@ int main()
 		{
 			scanf_s("%d", &length);
 			if ((length > 5) || (length < 2))
-				printf("You entered wrong length. It should be between 2 and 5!\n");
+				printf("You have entered an incorrect length. It should be between 2 and 5!\n");
 		} while ((length > 5) || (length < 2));
 
 		for (i = 0; i <= 9; i++)
@@ -75,7 +96,13 @@ int main()
 		do
 		{
 			printf("Enter the supposed number\n");
-			scanf_s("%d", &scanned);
+			do
+			{
+				scanf_s("%d", &scanned);
+				flag = scanned_check(scanned);
+				if (flag == false)
+					printf("You have entered an incorrect number. You should enter the number without repeated digits!\n");
+			} while (flag == false);
 
 			biks_and_cows(scanned, length);
 			attempts++;
