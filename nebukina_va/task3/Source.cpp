e2fc4ac78b946b43cd7b6146d36c43ba35ebdc2f
NebukinaVA <43473978+NebukinaVA@users.noreply.github.com>
2018-10-20 19:20:21 +0300

    Update Source.cpp

--- nebukina_va/task3/Source.cpp
+++ nebukina_va/task3/Source.cpp
@@ -4,60 +4,71 @@
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
-
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	int n, num, i, j; //n - длина числа
-	int cow = 0;
-	int bull = 0;
-	int comp[5]; // число компьютера
-	int number[5]; // число игрока
-	int d, a; // d - делитель
+	int n, num, i, j, end; //n - длина числа, num - число игрока, i, j - счетчики для циклов
+	int cow = 0; // количество коров
+	int bull = 0; // количество быков
+	int comp[5] = { 0 }; // массив для числа компьютера
+	int number[5] = { 0 }; // массив для числа игрока
+	int d, a; // d - делитель, a - вспомогательная переменная
+	int attempt = 0;
 	srand(time(NULL));
-	printf("Игра 'Быки и коровы'!\n Выберите длину загадываемого числа.\n Введите 2, 3, 4 или 5.\n");
+	printf("Игра 'Быки и коровы'!\nВыберите длину загадываемого числа.\nВведите 2, 3, 4 или 5.\n");
 	scanf_s("%i", &n);
-	while ((n < 2) && (n > 5))
+	while ((n < 2) || (n > 5))
 	{
-		printf("Количество разрядов может быть только 2, 3, 4 или 5.\n");
+		printf("Количество разрядов может быть только 2, 3, 4 или 5. Введите длину заново.\n");
 		scanf_s("%i", &n);
 	}
-	comp[0] = (rand() % 9 + 1);
-	for (i = 1; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		comp[i] = (rand() % 9 + 1);
-		while (comp[i] == comp[i - 1])
+		for (j = 0; j < i; j++)
 		{
-			comp[i] = (rand() % 9 + 1);
+			while (comp[i] == comp[j])
+			{
+				comp[i] = (rand() % 9 + 1);
+			}
 		}
 	}
 	printf("Введите Ваш вариант числа с неповторяющимися цифрами.\n");
 	scanf_s("%i", &num);
 	while (bull != n)
 	{
-		for (i = 0; i < n; i++);
+		while ((num <= pow(10, (n-1))) || (num >= pow(10, (n + 1))))
 		{
-			a = num;
-			d = pow(10, n - 1);
+			printf("Число не входит в выбранный диапазон!\n");
+			scanf_s("%i", &num);
+		}
+		attempt++;
+		a = num;
+		for (i = n - 1; i >= 0; i--)
+		{
+			d = pow(10, i);
 			number[i] = a / d;
 			a %= d;
 		}
+		bull = 0;
+		cow = 0;
 		for (i = 0; i < n; i++)
 		{
 			if (number[i] == comp[i])
 				bull++;
-		}
-		for (i = 0; i < n; i++)
-		{
-			for (j = 1; j < n; j++)
-			{
+			else
+				for (j = 0; j < n; j++)
+			    {
 				if ((i != j)&&(number[i] == comp[j]))
 					cow++;
-			}
+			    }
 		}
+		if (bull == n)
+			break;
 		printf("Быков: %i\nКоров: %i\n", bull, cow);
 		scanf_s("%i", &num);
 	}
-	printf("Вы угадали! Это число %i.\n", num);
-
+	printf("В самом деле! Это число %i. Количество попыток: %i\n", num, attempt);
+	scanf_s("%i", &end);
+	return 0;
 }
