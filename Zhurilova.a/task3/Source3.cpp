6e1a1a2960c6486c0c566b6477c45d1d57d1b213
Zhurilovaa <Anastasiya.Zhurilova@yandex.ru>
2018-10-20 20:21:04 +0300

    Устранение неисправности игры Task3

--- Zhurilova.a/task3/Source3.cpp
+++ Zhurilova.a/task3/Source3.cpp
@@ -10,13 +10,12 @@ void main()
 	setlocale(LC_ALL, "Rus");
 
 	int exit;
-	int n, dl, kolK, kolB;
-	int x, y, i, j;
-	int vvod1=0, vvod2, covp, proverka, game;
-	int gen[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int n, gran, kolK=0, kolB=0;
+	int  i, j;
+	int proverka;
 	int chisO[5] = { 0 };
 	int chisZ[5] = { 0 };
-	int zag=0, otg, zapasO;
+	int zag=0, otg, zapas;
 
 	do
 	{
@@ -24,117 +23,111 @@ void main()
 		scanf("%d", &n);
 	}
 	while ((n < 2) || (n > 5));
-	
-	
-	y = 10;
-	
-	for (i = 1; i <= n; i++)
+
+	proverka = 1;
+
+	while (proverka != 0)
 	{
-		x = rand() % (10);
-		if (y != x)
+
+		proverka = 0;//Если число создасться верно выход из цикла генерации обеспечен
+
+		for (i = 1; i <= n; i++)//Генерация числа
 		{
-			zag = zag * 10 + x;
-			y = x;
-			chisZ[i] = gen[x];
+
+			if (i == (n - 1))//Если i старший разряд, значит i не может быть равно 0
+			{
+				chisZ[i] = 1 + rand() % (9);//От 1 до 9
+			}
+			else
+			{
+				chisZ[i] = rand() % (10);//От 0 до 9
+			}
+
+		}
+
+		for (i = 0; i < n; i++)//Проверка на совпадение цифр
+		{
+			for (j = (i + 1); j < n; j++)//Нельзя сравнивать элементы одний позиции
+			{
+				if (chisZ[i] == chisZ[j])
+				{
+					proverka = 1;//Снова зайдет в цикл генерации
+					break;//Дальнейшая проверка бессмысленна
+				}
+			}
 		}
 
 	}
-	game = 0;
-	while (game == 0)
+	gran = 1;
+	for (i = 0; i < n; i++)//Вычисление границ предполагаемого числа согласно его границам
 	{
-		while (vvod1 == 0)
-		{
+		gran = gran * 10;
+	}
+	
+	while (kolB != n)
+	{
+		proverka = 1;
 
-			printf("Введите число\nВНИМАНИЕ!!! Число должно быть той же длины, что вы ввели раньше\nЧисло должно быть с РАЗЛИЧНЫМИ ЦИФРАМИ!\n");
-			scanf("%otg", &otg);
-			zapasO = otg;
-			dl = 1;
-			
-			while(zapasO > 0)
+		do
+		{
+			printf("Введите число.\nВНИМАНИЕ!!!\nДлина числа должна совпадать с введеной вами ранее и цифры в числе должны быть РАЗЛИЧНЫМИ\n");
+			scanf("%d", &otg);
+			zapas = otg;
+
+			if ((otg<(gran/10))||(otg>(gran-1)))
 			{
-				zapasO = zapasO / 10;
-				dl++;
-			}
-			zapasO = otg;
-			for (i = 1; i <= dl; i++)
-			{
-				chisO[i] = zapasO % 10;
-				zapasO = zapasO / 10;
-			}
-			if (dl != n)
-			{
-				printf("Сказано было ввести чиcло ОДИНАКОВОЙ длины!\n");
-				vvod2 = 0;
+				printf("Число не той длины!!!\n");
+				proverka = 1;
 			}
 			else
-				if (dl == n)
-				{
-					vvod2 = 1;
-				}
-
-			while (vvod2 == 1)
+				
 			{
-				covp = 0;
-				for (i = 1; i <= dl; i++)
+				proverka = 0;//Число верной длины				
+			}
+			for (i = 0; i < n; i++)//Заполнение массива с цифрами отгадки
+			{
+				chisO[i] = zapas % 10;
+				zapas = zapas / 10;
+			}
+			for (i = 0; i < n; i++)//Проверка на повторяемость цифр в отгадке
+			{
+				for (j = (i + 1); j < n; j++)
 				{
-					for (j = (i + 1); j <= dl; j++)
+					if (chisO[i] == chisO[j])
 					{
-						if (chisO[i] != chisO[j])
-						{
-							covp = 0;
-
-						}
-						else
-							if (chisO[i] == chisO[j])
-							{
-								covp = 1;
-								
-							}
-
+						proverka = 1;
+						printf("Вы ввели число с повторяющимися цифрами!!!\n");
+						break;
 					}
-					if (covp = 1)
-					{
-						printf("Было сказано с РАЗЛИЧНЫМИ цифрами!\n");
-						vvod2 = 0;
-						
-					}
-					else
-						if (covp = 0)
-						{
-							printf("Молодец! Вы ввели число верно!!!");
-							proverka = 0;
-						}
+				}
+				if (proverka == 1)//Если встретились повторяющиеся элементы закрыть цикл. Начать сначала
+				{
+					break;
 				}
 			}
-		}
+			
+		} while (proverka!=0);
 
-		if (proverka == 0)
+		kolK = 0;
+		kolB = 0;
+
+		for (i = 0; i < n; i++)
 		{
-			kolK = 0;
-			kolB = 0;
-			for (i = 1; i <= n; i++)
+			for (j = 0; j < n; j++)
 			{
-				for (j = 1; j <= n; j++)
+				if (chisZ[i] == chisO[j])//Если совпали цифры
 				{
-					if (chisZ[i] == chisO[j])
-					{
-						kolK++;
-					}
-					if ((chisZ[i] == chisO[j]) && (i == j))
+					kolK++;
+					if (i == j)//Если совпали их позиции
 					{
 						kolB++;
 					}
 				}
 			}
-			printf("Количество коров %d, Количество быков %d\n", kolK, kolB);
-			if (kolB == n)
-			{
-				printf("Молодец, вы угадали число!");
-				proverka = 1;
-				game = 1;
-			}
 		}
+		printf("Количестов коров %d, Количество быков %d\n", kolK, kolB);
 	}
-	
+
+	printf("Поздравляю!!! Вы угадали число!\n");
 	scanf("%d", &exit);
 }
